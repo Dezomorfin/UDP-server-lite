c945cf81a5ff0fdfa609130bc4bf302f5cfe195d
Dr.Vaggo <39808040+Dezomorfin@users.noreply.github.com>
2018-12-23 12:09:15 +0400

    Add files via upload

---
+++ UDPServer.java
@@ -0,0 +1,32 @@
+package udpserver;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UDPServer {
+
+    public static void main(String[] args) {
+        
+        try (DatagramSocket dServer = new DatagramSocket(1000);) {
+            
+            byte[] buff = new byte[128];
+            
+            DatagramPacket p = new DatagramPacket(buff, buff.length);
+            
+            dServer.receive(p);
+            
+            String message = new String(p.getData(), 0, p.getLength());
+            System.out.println(message);
+            
+        } catch (SocketException ex) {
+            System.out.println("Error in connection: " + ex.getMessage());
+        } catch (IOException ex) {
+            Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
